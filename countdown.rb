4f32eb5403c6ab403e279834c89be916d2d63b9e
Michael Boroski <mjboroski@gmail.com>
2018-04-19 19:12:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,10 @@
 def countdown(number)
   while number
-    number>0 ? puts "#{number} SECOND(S)!" : puts "HAPPY NEW YEAR!"
-    number+=1
+    if number>0 
+      puts "#{number} SECOND(S)!"
+    else
+      puts "HAPPY NEW YEAR!"
+    end  
+    number-=1
   end
 end
